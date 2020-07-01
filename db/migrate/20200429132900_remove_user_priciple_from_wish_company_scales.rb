bbb7374932f0a904bd6090e2760afefa50f78fcd
shota27182 <shota27182@gmail.com>
2020-07-01 03:27:18 +0000

    　eleventh-push

--- db/migrate/20200429132900_remove_user_priciple_from_wish_company_scales.rb
+++ db/migrate/20200429132900_remove_user_priciple_from_wish_company_scales.rb
@@ -1,5 +1,5 @@
 class RemoveUserPricipleFromWishCompanyScales < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :wish_company_scales, :user_priciple, foreign_key: true
+    remove_reference :wish_company_scales, :user_priciple
   end
 end
