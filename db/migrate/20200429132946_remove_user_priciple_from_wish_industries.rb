18cf0edde776eb2e91edff8ed5e2b258492c6103
shota27182 <shota27182@gmail.com>
2020-07-01 03:31:11 +0000

    　twelveth-push

--- db/migrate/20200429132946_remove_user_priciple_from_wish_industries.rb
+++ db/migrate/20200429132946_remove_user_priciple_from_wish_industries.rb
@@ -1,5 +1,5 @@
 class RemoveUserPricipleFromWishIndustries < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :wish_industries, :user_priciple, foreign_key: true
+    remove_reference :wish_industries, :user_priciple
   end
 end
