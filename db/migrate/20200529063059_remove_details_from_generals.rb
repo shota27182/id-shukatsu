be45c8cffe76f09c5c740e7947066793da7a7555
shota27182 <shota27182@gmail.com>
2020-07-01 05:19:07 +0000

    　twenty-nineth-push

--- db/migrate/20200529063059_remove_details_from_generals.rb
+++ db/migrate/20200529063059_remove_details_from_generals.rb
@@ -1,11 +1,9 @@
 class RemoveDetailsFromGenerals < ActiveRecord::Migration[5.1]
   def up
-    remove_column :generals, :user
     remove_column :generals, :profile
   end
 
   def down
-    add_column :generals, :user, :references
     add_column :generals, :profile, :references
   end
 end
