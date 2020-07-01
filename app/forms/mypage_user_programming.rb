2630e1f8c969336d51234358042b348dc5efe6ae
shota27182 <shota27182@gmail.com>
2020-07-01 01:25:39 +0000

    　fifth-push

--- app/forms/mypage_user_programming.rb
+++
@@ -1,7 +0,0 @@
-class MypageUserProgramming
-  include ActiveModel::Model
-    attribute :user_id, :integer
-    attribute :programming_id, :integer
-    attribute :use_time, :string
-    attribute :description, :string
-end
