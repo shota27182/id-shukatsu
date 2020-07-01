60291f99d47a2fb11417ddad12f61e9402d886a6
shota27182 <shota27182@gmail.com>
2020-07-01 05:54:24 +0000

    　thirteenth-second-push

--- db/migrate/20200622104605_create_grades.rb
+++ db/migrate/20200622104605_create_grades.rb
@@ -1,7 +1,6 @@
 class CreateGrades < ActiveRecord::Migration[5.2]
   def change
     create_table :grades do |t|
-      t.references :name, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200622111246_remove_name_from_grade.rb
+++ db/migrate/20200622111246_remove_name_from_grade.rb
@@ -1,5 +1,4 @@
 class RemoveNameFromGrade < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :grades, :name, foreign_key: true
   end
 end
