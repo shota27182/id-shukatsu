0a4e280b2578f65f22ae2a400dd70f926ab3fbb0
shota27182 <shota27182@gmail.com>
2020-07-01 03:11:10 +0000

    　eighth-push

--- db/migrate/20200429075744_add_details_to_universities.rb
+++ db/migrate/20200429075744_add_details_to_universities.rb
@@ -1,5 +1,4 @@
 class AddDetailsToUniversities < ActiveRecord::Migration[5.1]
   def change
-    add_reference :universities, :user_join, foreign_key: true
   end
 end
--- db/migrate/20200429075803_add_details_to_faculties.rb
+++ db/migrate/20200429075803_add_details_to_faculties.rb
@@ -1,5 +1,4 @@
 class AddDetailsToFaculties < ActiveRecord::Migration[5.1]
   def change
-    add_reference :faculties, :user_join, foreign_key: true
   end
 end
