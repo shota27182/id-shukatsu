aac686d0c576580163d1c2df16190111b371348d
shota27182 <shota27182@gmail.com>
2020-07-14 16:32:10 +0000

    one-commit

--- db/migrate/20200712121359_remove_event_from_notifications.rb
+++ db/migrate/20200712121359_remove_event_from_notifications.rb
@@ -1,5 +1,5 @@
 class RemoveEventFromNotifications < ActiveRecord::Migration[5.2]
   def change
-    remove_reference :notifications, :event, foreign_key: true
+    remove_reference :notifications, :event
   end
 end
