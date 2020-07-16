9904e07d92290b9c72eca4698dcba7598296f1b9
shota27182 <shota27182@gmail.com>
2020-07-16 06:23:13 +0000

    d

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,24 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "id_shukatsu_#{Rails.env}"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  host = 'https://www.id-shukatsu.com'
+  config.action_mailer.default_url_options = {host: host}
+  # development.rbだと、上の２行じゃなくて下の一行
+  # config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV["SENDGRID_USERNAME"],
+      :password => ENV["SENDGRID_PASSWORD"],
+      :domain => 'id-shukatsu.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
