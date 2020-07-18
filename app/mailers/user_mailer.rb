d40bafc0e95e9edf0029d05de9fbd51aaba9ec83
shota27182 <shota27182@gmail.com>
2020-07-18 05:54:56 +0000

    p

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,10 +5,19 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.account_activation.subject
   #
+  default from: 'info@id-shukatsu.com'
+  
+  
   def account_activation(user)
     @user = user
 
     mail to: user.email, subject: "Account activations"
+    
+    mail( from: '"送信者名" <info@独自ドメイン.com>',
+          to: @user.email,
+          subject: 'お問い合わせありがとうございます。',
+          bcc: 'shota27182@gmai.com',
+          reply_to: 'info@id-shukatsu.com' )
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
