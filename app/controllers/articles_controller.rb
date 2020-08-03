d36ad49faa5885e20c68b8cc95e304c5136ce009
shota27182 <shota27182@gmail.com>
2020-08-03 12:36:50 +0000

    7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,14 +4,29 @@ class ArticlesController < ApplicationController
     @WorkAreas = WorkArea.all
     @occupations = Occupation.all
     
-    if params[:industry]
-      @IndustriesSearch = params[:industry].split(",").map(&:to_i)
+    if params[:category] == '就活初心者向けコンテンツ' && !params[:sub_category]
+      @article = Article.includes(:tags).where(tags:{id: 1..3})
     end
-    if params[:area]
-      @AreasSearch = params[:area].split(",").map(&:to_i)
+    if params[:category] == '業界別コンテンツ' && !params[:sub_category]
+      @article = Article.includes(:tags).where(tags:{id: 4..18})
     end
-    if params[:occupation]
-      @OccupationsSearch = params[:occupation].split(",").map(&:to_i)
+    if params[:category] == '選考対策コンテンツ' && !params[:sub_category]
+      @article = Article.includes(:tags).where(tags:{id: 19..25})
+    end
+    if params[:category] == '所属別コンテンツ' && !params[:sub_category]
+      @article = Article.includes(:tags).where(tags:{id: 25..30})
+    end
+    if params[:category] == 'キャリアコンテンツ' && !params[:sub_category]
+      @article = Article.includes(:tags).where(tags:{id: 30..33})
+    end
+    if params[:category] == '内定者向けコンテンツ' && !params[:sub_category]
+      @article = Article.includes(:tags).where(tags:{id: 33..35})
+    end
+    if params[:category] == 'その他のコンテンツ' && !params[:sub_category]
+      @article = Article.includes(:tags).where(tags:{id: 35..37})
+    end
+    if params[:sub_category]
+      @article = Article.includes(:tags).where(tags:{name: params[:sub_category]})
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_031256) do
+ActiveRecord::Schema.define(version: 2020_08_03_121222) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,15 @@ ActiveRecord::Schema.define(version: 2020_07_30_031256) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+  end
+
+  create_table "article_tags", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_article_tags_on_article_id"
+    t.index ["tag_id"], name: "index_article_tags_on_tag_id"
   end
 
   create_table "articles", force: :cascade do |t|
@@ -361,6 +370,12 @@ ActiveRecord::Schema.define(version: 2020_07_30_031256) do
   end
 
   create_table "programmings", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
