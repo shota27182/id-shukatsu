81d855427211b1d9fa602974854bccbd7373208d
shota27182 <shota27182@gmail.com>
2020-09-13 08:18:05 +0000

    1

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -1,6 +1,6 @@
 class Salary < ApplicationRecord
   belongs_to :company
-  belongs_to :first_salary
-  belongs_to :average_salary
+  belongs_to :first_salary, optional: true
+  belongs_to :average_salary, optional: true
   
 end
